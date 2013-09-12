a87e056dbb1f6c3fc42352586b4e92cade1fb062
fgsoap <fgsoap@gmail.com>
2013-09-12 16:21:05 +0800

    Update hp_msm760_auto_restart_aps.py

--- hp_msm760_auto_restart_aps.py
+++ hp_msm760_auto_restart_aps.py
@@ -2,34 +2,36 @@
 # -*- coding: utf-8 -*-
 # Copyright 2013 fgsoap
 # All Rights Reserved.
+try:
+         import requests, time
+except ImportError:
+         print "Please make sure the requests is installed!"
 
-import requests, time
-
-real_ip="your_ip"
-
+# please change the "ip" into your real ip.
 headers={
-         'Referer':'https://real_ip/home.asp',
-         'Host':'real_ip','User-Agent':'Mozilla/5.0 (Windows NT 5.1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/29.0.1547.66 Safari/537.36',
+         'Referer':'https://"ip"/home.asp',
+         'Host':"ip",'User-Agent':'Mozilla/5.0 (Windows NT 5.1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/29.0.1547.66 Safari/537.36',
          }
 login_data={
+#Please change them.
             'username':'admin',
             'pw':'admin'
             }
 datas={
-       #'Reset':#'Restart'
+       'Reset':'Restart'
        }
 macs={
-       '10F-2':"00:24:A8:89:77:BC",
-    
-
+       'ap_name1':"00:24:A8:89:77:AC",
+       'ap_name2':"00:24:A8:89:77:AB",
+# you can add a lots of apps here      
       }
-s.post(url='https://real_ip/goform/Logout',verify=False,headers=headers,data=login_data)
+s.post(url='https://"ip"/goform/Logout',verify=False,headers=headers,data=login_data)
 for mac in macs:
     realmac=macs.get(mac)
-    url='https://real_ip/centcfg/ap_overview_details.asp?entity=device&selector=%s&product=20' % realmac
+    url='https://"ip"/centcfg/ap_overview_details.asp?entity=device&selector=%s&product=20' % realmac
     s.get(url,verify=False)
-    s.get('https://real_ip/centcfg/maintenance.asp',verify=False)
-    s.post('https://real_ip/goform/FormDeviceSystemRestart',verify=False,data=datas)
+    s.get('https://"ip"/centcfg/maintenance.asp',verify=False)
+    s.post('https://"ip"/goform/FormDeviceSystemRestart',verify=False,data=datas)
     print mac + " " + "has been restarted!"
     time.sleep(1)
 
