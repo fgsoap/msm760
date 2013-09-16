10a141b37320758cdf8525172b437d4f689de06f
fgsoap <fgsoap@gmail.com>
2013-09-16 16:38:47 +0800

    Update hp_msm760_auto_restart_aps.py

--- hp_msm760_auto_restart_aps.py
+++ hp_msm760_auto_restart_aps.py
@@ -7,13 +7,13 @@ try:
 except ImportError:
          print "Please make sure the requests is installed!"
 
-# please change the "ip" into your real ip.
+# Please change the "ip" into your real ip.
 headers={
          'Referer':'https://"ip"/home.asp',
          'Host':"ip",'User-Agent':'Mozilla/5.0 (Windows NT 5.1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/29.0.1547.66 Safari/537.36',
          }
 login_data={
-#Please change them.
+# Please change them.
             'username':'admin',
             'pw':'admin'
             }
@@ -23,7 +23,7 @@ datas={
 macs={
        'ap_name1':"00:24:A8:89:77:AC",
        'ap_name2':"00:24:A8:89:77:AB",
-# you can add a lots of aps here      
+# You can add a lots of aps here.      
       }
 s.post(url='https://"ip"/goform/Logout',verify=False,headers=headers,data=login_data)
 for mac in macs:
