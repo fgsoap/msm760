9673375935d47b186b840aa7fd1aa45d7e048760
fgsoap <fgsoap@gmail.com>
2013-09-16 17:59:04 +0800

    Update hp_msm760_auto_restart_aps.py
    Get the dict of all the aps automaticlly.

--- hp_msm760_auto_restart_aps.py
+++ hp_msm760_auto_restart_aps.py
@@ -4,12 +4,13 @@
 # All Rights Reserved.
 try:
          import requests, time
+         from pyquery import PyQuery as pq
 except ImportError:
-         print "Please make sure the requests is installed!"
+         print "Please make sure the Requests and the PyQuery are installed!"
 
 # Please change the "ip" into your real ip.
 headers={
-         'Referer':'https://"ip"/home.asp',
+         'Referer':'https://ip/home.asp',
          'Host':"ip",'User-Agent':'Mozilla/5.0 (Windows NT 5.1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/29.0.1547.66 Safari/537.36',
          }
 login_data={
@@ -20,18 +21,30 @@ login_data={
 datas={
        'Reset':'Restart'
        }
-macs={
-       'ap_name1':"00:24:A8:89:77:AC",
-       'ap_name2':"00:24:A8:89:77:AB",
-# You can add a lots of aps here.      
-      }
-s.post(url='https://"ip"/goform/Logout',verify=False,headers=headers,data=login_data)
-for mac in macs:
-    realmac=macs.get(mac)
-    url='https://"ip"/centcfg/ap_overview_details.asp?entity=device&selector=%s&product=20' % realmac
+# Login
+s=requests.Session()
+s.post(url='https://ip/goform/Logout',verify=False,headers=headers,data=login_data)
+
+# Get the dict of all the aps.
+n=s.get('https://ip/centcfg/ap_overview.asp?entity=vgroup&selector=Synchronized',verify=False)
+html=pq(n.text)
+ap_name=[]
+ap_mac=[]
+i_a=html("li div a")
+i_span=html("li div a span")
+for i in xrange(len(i_a)):
+    ap_name.append(i_span.eq(i).text())
+    ap_mac.append(i_a.eq(i).attr("href").strip("&amp;product=20").strip("centcfg/ap_overview_details.asp?entity=device&selector=").replace("%3A", ":"))
+
+aps=dict(zip(ap_name,ap_mac))
+
+#Let's do the restart!
+for mac in aps:
+    realmac=aps.get(mac)
+    url='https://ip/centcfg/ap_overview_details.asp?entity=device&selector=%s&product=20' % realmac
     s.get(url,verify=False)
-    s.get('https://"ip"/centcfg/maintenance.asp',verify=False)
-    s.post('https://"ip"/goform/FormDeviceSystemRestart',verify=False,data=datas)
+    s.get('https://ip/centcfg/maintenance.asp',verify=False)
+    s.post('https://ip/goform/FormDeviceSystemRestart',verify=False,data=datas)
     print mac + " " + "has been restarted!"
     time.sleep(1)
 
